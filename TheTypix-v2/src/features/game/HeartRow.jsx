186425a85cf9999bfa40c6bf44a9016165fc76b6
tariksaid-dev <tariksaid.dev@gmail.com>
2023-09-16 03:33:25 +0200

    Hearth functionality fixed

--- TheTypix-v2/src/features/game/HeartRow.jsx
+++ TheTypix-v2/src/features/game/HeartRow.jsx
@@ -1,11 +1,14 @@
+import { useSettings } from "../context/SettingsContext";
 import Heart from "./Heart";
 
 function HeartRow() {
+  const { lives } = useSettings();
+
   return (
     <div className="flex bg-black">
-      <Heart />
-      <Heart />
-      <Heart />
+      {Array.from({ length: lives }, (_, index) => (
+        <Heart key={index} />
+      ))}
     </div>
   );
 }
